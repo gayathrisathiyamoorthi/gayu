a2c229173cb77c815518d7f006ffe81024aa5633
gayathrisathiyamoorthi <gayathri.s1313029@ksrce.ac.in>
2016-08-12 15:58:23 +0530

    Add files via upload

--- Reverse.java
+++ Reverse.java
@@ -1,18 +1,25 @@
-package com.wipro.cse;
+package com.guvi.logic;
 
 import java.util.Scanner;
 
-public class Reverse {
-  public static void main(String[] args) {
-	  String a="";
-	  Scanner scanner=new Scanner(System.in);
-	  String s=scanner.next();
-	  for(int i=s.length()-1;i>=0;i--){
-		a=a+s.charAt(i);  
-	  }
-	  System.out.println(a);
-		
-
+public class Snippet {
+	public static void main(String[] args) {
+			String s, a = "";
+			System.out.println("enter the string");
+			Scanner scanner = new Scanner(System.in);
+			s = scanner.next();
+			for (int i = s.length() - 1; i >= 0; i--) {
+				if(i==s.length()-1){
+					a=a+s.charAt(i);
+				}else{
+				   a = a +'-'+ s.charAt(i);
+				}
+			}
+			System.out.println(a);
+			
+		}
+	
 	}
+	
 
-}
+
