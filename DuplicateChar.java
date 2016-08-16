96fbb2379d90f8602fad81e8f2915bb8f0405941
gayathrisathiyamoorthi <gayathri.s1313029@ksrce.ac.in>
2016-08-16 19:29:21 +0530

    Add files via upload

---
+++ DuplicateChar.java
@@ -0,0 +1,24 @@
+package com.wipro.cse;
+
+import java.util.Scanner;
+
+public class DuplicateChar {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		String s=scanner.next();
+		int count=0;
+		for(int i=0;i<s.length();i++){
+			for(int j=i+1;j<s.length();j++){
+			if(s.charAt(i)==s.charAt(j)){
+				count++;
+			}
+			}
+		}
+		System.out.println(s.length());
+		int c=s.length()-1-count;
+		System.out.println(c);
+
+	}
+
+}
